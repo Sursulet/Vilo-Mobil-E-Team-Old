02362ef57004b7f0e2e6f638debce78b1a3a3032
Sursulet <57486825+Sursulet@users.noreply.github.com>
2021-12-07 15:07:52 +0100

    Initial Commit

---
+++ Vilo Mobil E Team/Vilo_Mobil_E_TeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vilo_Mobil_E_TeamApp.swift
+//  Vilo Mobil E Team
+//
+//  Created by Apprenant 85 on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Vilo_Mobil_E_TeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
