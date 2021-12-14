2be4e5d72f494e21bb4a141987328f21507f08a8
guiltyflower <giovanni.fioretto.23@gmail.com>
2021-12-14 13:19:21 +0100

    lololololololo

---
+++ Vilo Mobil E Team/View/Extracted/Gioview.swift
@@ -0,0 +1,20 @@
+//
+//  Gioview.swift
+//  Vilo Mobil E Team
+//
+//  Created by Giovanni Fioretto on 14/12/2021.
+//
+
+import SwiftUI
+
+struct Gioview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Gioview_Previews: PreviewProvider {
+    static var previews: some View {
+        Gioview()
+    }
+}
