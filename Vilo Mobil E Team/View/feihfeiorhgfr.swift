ef4eb5373979ae0060892e90aab81a98acd53271
max6oux <91872709+max6oux@users.noreply.github.com>
2021-12-14 13:19:19 +0100

    fezjklvehfioerzbiosvmdbvpeziodfbhvpouzerm noergoierhvioer

---
+++ Vilo Mobil E Team/View/feihfeiorhgfr.swift
@@ -0,0 +1,20 @@
+//
+//  feihfeiorhgfr.swift
+//  Vilo Mobil E Team
+//
+//  Created by Maxime Bégué on 14/12/2021.
+//
+
+import SwiftUI
+
+struct feihfeiorhgfr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct feihfeiorhgfr_Previews: PreviewProvider {
+    static var previews: some View {
+        feihfeiorhgfr()
+    }
+}
