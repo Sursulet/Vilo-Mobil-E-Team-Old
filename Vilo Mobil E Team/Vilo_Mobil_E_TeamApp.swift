63e7686e9fcb4dfe271f99becf94d4f4741e7523
max6oux <91872709+max6oux@users.noreply.github.com>
2021-12-14 13:43:55 +0100

    splash screen

--- Vilo Mobil E Team/Vilo_Mobil_E_TeamApp.swift
+++ Vilo Mobil E Team/Vilo_Mobil_E_TeamApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Vilo_Mobil_E_TeamApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
